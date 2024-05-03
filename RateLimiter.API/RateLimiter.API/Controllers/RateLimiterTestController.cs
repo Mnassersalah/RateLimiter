f570055d1a3130b48a1cfca9a67c755f18551e30
Nasser Salah El-Din <mnassers@advent.com>
2024-05-03 22:14:15 +0300

    add testing api project

---
+++ RateLimiter.API/RateLimiter.API/Controllers/RateLimiterTestController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RateLimiter.API.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class RateLimiterTestController : ControllerBase
+    {
+
+        [HttpGet("unlimited")]
+        public ActionResult<string> GetUnlimited()
+        {
+            return Ok("Unlimited! Let's Go!");
+        }
+
+        [HttpGet("limited")]
+        public ActionResult<string> GetLimited()
+        {
+            return Ok("Limited, don't over use me!");
+        }
+
+    }
+}
